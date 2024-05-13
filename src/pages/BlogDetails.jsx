d57ec9658f6e2c2c8dc3ef58b4f16127c5a5321c
MarGoN <monondcosta@gmail.com>
2024-05-14 05:11:43 +0600

    added comment section, fetching id specific comment

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
 import Loader from "../components/Loader";
+import useAuth from "../hooks/useAuth";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 
 const BlogDetails = () => {
 
     const { id } = useParams();
-
+    const { user } = useAuth();
+    const [getComments, setGetComments] = useState([]);
     const [blogDetail, setBlogDetail] = useState({});
 
     useEffect(() => {
@@ -31,7 +35,61 @@ const BlogDetails = () => {
     //     setBookDetails(singleBook);
     // }, [book, id]);
 
-    
+    // user info
+    const userName = user?.displayName;
+    const userEmail = user?.email;
+    const userPhoto = user?.photoURL;
+    const postID = id;
+
+
+    //get comments
+    useEffect(() => {
+        axios(`${import.meta.env.VITE_SERVER}/getComments`)
+
+            .then(data => {
+                setGetComments(data.data);
+                // console.log(data);
+            })
+    }, [getComments]);
+
+    // Filter comments based on postID
+    const filteredComments = getComments.filter(com => com.postID === id);
+
+
+    // console.log('comments', getComments)
+
+    // comment adder
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const comments = form.comments.value;
+
+        const commenter = { userName, userEmail, userPhoto, comments, postID };
+
+        //send data to server
+        axios.post(`${import.meta.env.VITE_SERVER}/addComment`, commenter)
+
+            .then(data => {
+                // console.log(data); 
+                if (data.data?.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Comment added! 🎉',
+                        icon: 'success',
+                        confirmButtonText: 'Cool 😎'
+                    }).then(() => {
+                        form.reset(); // Reset the form fields
+                    });
+                }
+            })
+    }
+
+
+
+
+
+
     // loader
     const [loading, setLoading] = useState(true);
 
@@ -49,45 +107,94 @@ const BlogDetails = () => {
 
 
     return (
-        <div className="card card-side bg-base-100 shadow-2xl lg:card-side  border-2">
+        <>
+            <div className="card card-side bg-base-100 shadow-2xl lg:card-side  border-2">
 
-            <Helmet>
-                <title>Word Insight | Blog Details</title>
-            </Helmet>
+                <Helmet>
+                    <title>Word Insight | Blog Details</title>
+                </Helmet>
 
-            <figure className="object-contain w-1/2 ">
-                <img
-                    className="h-full "
-                    src={photo} alt="" />
-            </figure>
-            <div className="card-body  w-1/2">
-                <h2 className="card-title text-xl font-semibold text-start font-serif">
-                    {title}
-                </h2>
-                <p className="text-base">Category :{' '}
-                    <span className='font-semibold badge badge-primary'>
-                        #{category}
-                    </span>
-                </p>
+                <figure className="object-contain w-1/2 ">
+                    <img
+                        className="h-full "
+                        src={photo} alt="" />
+                </figure>
+                <div className="card-body  w-1/2">
+                    <h2 className="card-title text-xl font-semibold text-start font-serif">
+                        {title}
+                    </h2>
+                    <p className="text-base">Category :{' '}
+                        <span className='font-semibold badge badge-primary'>
+                            #{category}
+                        </span>
+                    </p>
 
-                <div className="divider my-0 "></div>
+                    <div className="divider my-0 "></div>
 
-                <p>{short_description}</p>
+                    <p>{short_description}</p>
 
-                <div className="divider my-0 "></div>
+                    <div className="divider my-0 "></div>
 
-                <p>{long_description}</p>
+                    <p>{long_description}</p>
 
-                <div className="divider my-0 "></div>
+                    <div className="divider my-0 "></div>
 
-                <p className="text-base text-right">Added By : <span className='font-semibold'>
-                    {name} 
-                    {/* long_description display {long_description.length} */}
-                </span></p>
+                    <p className="text-base text-right">Added By : <span className='font-semibold'>
+                        {name}
+                        {/* long_description display {long_description.length} */}
+                    </span></p>
 
-                
+
+                </div>
             </div>
-        </div>
+
+            <div className="my-8 w-full">
+                <form onSubmit={handleSubmit}>
+                    <textarea
+                        className="textarea textarea-primary w-full py-4 px-6"
+                        name='comments'
+                        placeholder="comment here......."></textarea>
+                    <div className="w-full pr-2 flex justify-end">
+                        <button
+                            type="submit"
+                            className="btn btn-primary "
+                        >
+                            Comment
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            {/* get data */}
+            <div className="my-8 w-full p-4 border-2">
+                <h3 className="text-2xl text-center underline">Comment Section</h3>
+
+                {
+
+
+
+                    filteredComments.map((cmnt, index) => {
+                        return <div key={index} className="flex gap-4 space-y-9">
+                            <textarea
+                                disabled
+                                className="textarea textarea-primary py-4 my-4 px-6 w-full font-bold text-lg"
+                                name='comments'
+                                defaultValue={cmnt?.comments}></textarea>
+                            <div className="pr-2 flex flex-col gap-2 justify-center items-center">
+                                <p className="text-base font-bold font-serif">{cmnt?.userName}</p>
+                                <div className="avatar">
+                                    <div className="w-16 h-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                        <img src={cmnt?.userPhoto} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    })
+                }
+
+            </div>
+        </>
     );
 };
 
